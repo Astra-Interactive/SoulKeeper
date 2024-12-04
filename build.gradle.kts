f440929eb4927ea220e09ba0e34d22ae5639573c
Roman Makeev <makeevrserg@gmail.com>
2024-12-04 20:58:20 +0300

    fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,12 +12,10 @@ plugins {
     alias(libs.plugins.klibs.gradle.dokka.root) apply false
     alias(libs.plugins.klibs.gradle.dokka.module) apply false
     alias(libs.plugins.klibs.gradle.java.core) apply false
-    alias(libs.plugins.klibs.gradle.stub.javadoc) apply false
     alias(libs.plugins.klibs.gradle.publication) apply false
     alias(libs.plugins.klibs.gradle.rootinfo) apply false
-    // klibs - minecraft
-    alias(libs.plugins.klibs.gradle.minecraft.empty) apply false
-    alias(libs.plugins.klibs.gradle.minecraft.multiplatform) apply false
+    alias(libs.plugins.klibs.minecraft.shadow) apply false
+    alias(libs.plugins.klibs.minecraft.resource.processor) apply false
 }
 
 apply(plugin = "ru.astrainteractive.gradleplugin.dokka.root")
--- plugin/src/main/kotlin/ru/astrainteractive/soulkeeper/datetime/TimeAgoTranslationFormatter.kt
+++ plugin/src/main/kotlin/ru/astrainteractive/soulkeeper/datetime/TimeAgoTranslationFormatter.kt
@@ -4,7 +4,7 @@ import ru.astrainteractive.astralibs.string.StringDesc
 import ru.astrainteractive.soulkeeper.core.plugin.PluginTranslation
 
 internal class TimeAgoTranslationFormatter(private val translation: PluginTranslation) {
-    fun format(timeAgo: TimeAgoFormatter.Format): StringDesc.Raw {
+    fun format(timeAgo: TimeAgoFormatter.Format): StringDesc {
         return when (timeAgo) {
             is TimeAgoFormatter.Format.DayAgo -> translation.souls.daysAgoFormat(timeAgo.duration)
             is TimeAgoFormatter.Format.HourAgo -> translation.souls.hoursAgoFormat(timeAgo.duration)
