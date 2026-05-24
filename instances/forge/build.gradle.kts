899c3e8c8a8475fb966e82a695e4d38ef6ed7a57
Roman Makeev <makeevrserg@gmail.com>
2026-05-24 14:15:13 +0300

    [FIX] Fix licence

--- instances/forge/build.gradle.kts
+++ instances/forge/build.gradle.kts
@@ -50,6 +50,7 @@ minecraftProcessResource {
         customProperties = mapOf(
             "minecraft_version" to libs.versions.minecraft.forgeversion.get().split("-")[0],
             "forge_version" to libs.versions.minecraft.forgeversion.get().split("-")[1],
+            "mod_license" to "MIT License"
         )
     )
 }
@@ -87,7 +88,7 @@ val shadowJar by tasks.getting(ShadowJar::class) {
         // Root
         if (project.name == "forge" || project.name == "neoforge") {
             // Use kotlin-neoforge or kotlin-forge
-            exclude("kotlin/**")
+//            exclude("kotlin/**")
         }
         exclude("_COROUTINE/**")
         exclude("DebugProbesKt.bin")
